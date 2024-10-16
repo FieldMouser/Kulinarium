1606826c139ac78bbdba62fa54160a115b902be3
FieldMouser <vlad01.com98@gmail.com>
2024-10-17 00:37:23 +0700

    Added recipes addition and dedigned main page

--- addRecipe.php
+++ addRecipe.php
@@ -14,15 +14,24 @@
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-    /*
+
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $title = $_POST['title'];
         $ingredients = $_POST['ingredients'];
         $instructions = $_POST['instructions'];
+        $poster = isset($_COOKIE['login']) ? $_COOKIE['login']:'';
+        if (!empty($poster)){
+            $sql = $conn->prepare("INSERT INTO recipes (user_name, title, ingredients, instructions) VALUES (?, ?, ?, ?)");
+            $sql->execute([$poster, $title, $ingredients, $instructions]);
 
-        $stmt = $conn->prepare("INSERT INTO recipes (title, ingredients, instructions) VALUES (?, ?, ?)");
-        $stmt->execute([$title, $ingredients, $instructions]);
+        } 
+        else {
+            echo '<h1>Для добавления рецепта вам нужно войти в аккаунт!</h1><br> <a href="login.php">Войти</a> <br> или <br> 
+            <a href="registration.php">Войти</a>'; 
+        }
+        
+        
         
     }
-    */
+    
     ?>
--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+
+$login = isset($_COOKIE['login']) ? $_COOKIE['login'] : '';
+?>
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -10,7 +15,7 @@
     <header>
         <h1>Кулинарий!</h1>
                 <?php
-            $login = isset($_COOKIE['login']) ? $_COOKIE['login']:'';
+            
             if (!empty($login)){
                 echo "<p>Вы вошли как $login </p> <p><a href='logout.php'>(Выйти)</a></p> ";
             } 
@@ -23,10 +28,46 @@
     <div class="content">
         <center>
         <?php
-        if (!empty($login)){
-            echo '<p><a href="newRecipe.php">Создать свой рецепт</a></p>';
-        } 
-                ?>
+
+            
+
+            $serverName = "localhost";
+            $username = "u2852904_Login_E";
+            $password = "Adm1n_L0g1n_3254";
+            $dbName = "u2852904_default";
+
+            //создание соединения
+            $conn = new mysqli($serverName, $username, $password, $dbName);
+
+            // Проверка соединения
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+                    
+            $sql = $conn->query("SELECT * FROM recipes");
+
+            if ($sql && $sql->num_rows > 0) {
+                while ($recipe = $sql->fetch_assoc()) {
+                    echo "<div class='recipe-card'>";
+                    echo "<h2 class='recipe-title'>{$recipe['title']}</h2>";
+                    echo "<p class='recipe-ingredients'>Создал рецепт: {$recipe['user_name']}</p>";
+                    echo "<p class='recipe-ingredients'>Ингредиенты: {$recipe['ingredients']}</p>";
+                    echo "<p class='recipe-instructions'>Инструкции: {$recipe['instructions']}</p>";
+                    echo "</div>";
+                }
+            } else {
+                echo "<p>Нет рецептов для отображения.</p>";
+            }
+
+            if (!empty($login)){
+                echo '<p class="newRecipe"><a href="newRecipe.php">Создать свой рецепт</a></p>';
+            } 
+
+            $conn->close(); // Закрываем соединение
+        
+   
+                
+            ?>
         </center>
     </div>
     <footer><center><p>Пока еще тестовая страница</p></center></footer>
